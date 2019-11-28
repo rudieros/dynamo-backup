f592d988f27ec41f7e5e64a08bab364ac70104cd
Rudi Eros De Francesco <rudierosdfr@gmail.com>
2019-11-28 18:12:22 -0300

    init

---
+++ backupTables.ts
@@ -0,0 +1,33 @@
+import * as AWS from 'aws-sdk'
+import fs from 'fs'
+
+const dynamo = new AWS.DynamoDB({ region: 'us-east-1' })
+const db = new AWS.DynamoDB.DocumentClient({
+  region: 'us-east-1',
+})
+
+const backup = async () => {
+  const tables = await dynamo.listTables().promise()
+  if (!tables.TableNames) {
+    return
+  }
+  await Promise.all(tables.TableNames.map(async (TableName: any) => {
+    if (!TableName.toLowerCase().includes('prod')) {
+      return
+    }
+    let lastKey: any = undefined
+    const items: any[] = []
+    do {
+      const fetchedItems = await db.scan({
+        TableName,
+        ExclusiveStartKey: lastKey,
+      }).promise()
+      items.push(fetchedItems.Items)
+      lastKey = fetchedItems.LastEvaluatedKey
+    } while (lastKey)
+    fs.writeFileSync(`./backups/${TableName}.json`, JSON.stringify(items))
+  }))
+}
+
+backup()
+
